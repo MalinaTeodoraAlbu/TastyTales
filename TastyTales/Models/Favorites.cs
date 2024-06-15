bb1e0307125dbb7752688309e5ae9b923a710388
Malina Teodora <albumalina26@gmail.com>
2024-06-15 10:12:24 +0300

    Add project files.

---
+++ TastyTales/Models/Favorites.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TastyTales.Models
+{
+    public class Favorites
+    {
+        
+        public List<Recipe> Recipes { get; set; } = new List<Recipe>();
+    }
+}
---
+++ TastyTales/Models/Recipe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TastyTales.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string MealName { get; set; }
+        public Category Category { get; set; } = new Category();
+    }
+}
