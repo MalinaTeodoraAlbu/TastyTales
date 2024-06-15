f7638d67af40bc11e96e78938e246e4bf19dd686
Malina Teodora <albumalina26@gmail.com>
2024-06-15 14:42:40 +0300

    constants

--- TastyTales/Utilities/Constants.cs
+++ TastyTales/Utilities/Constants.cs
@@ -9,5 +9,6 @@ namespace TastyTales.Utilities
     public class Constants
     {
         public const string DatabaseFile = "Recipes.db";
+        public const string StringSearchByName = "www.themealdb.com/api/json/v1/1/search.php?s=";
     }
 }
